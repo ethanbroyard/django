4adeb257f5d082b266056ad02f93d08451d8de88
V.Bernardi <vincent.bernardi@etu.univ-orleans.fr>
2023-09-06 15:48:40 +0200

    [ADD] Ajout polls

--- GestionTaches/GestionTaches/settings.py
+++ GestionTaches/GestionTaches/settings.py
@@ -103,14 +103,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
-
+DATE_INPUT_FORMATS = ('%d/%m/%Y','%Y-%m-%d')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
